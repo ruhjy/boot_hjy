6cf9cc4eb2bb756d608d9dedaab63a8fd646e936
ruhjy <108906280+ruhjy@users.noreply.github.com>
2023-04-14 15:45:15 +0900

    Dto

---
+++ src/main/java/com/example/demo/domain/Customer3.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+@Data
+public class Customer3 {
+	private int id;
+	private String name;
+	private String address;
+}
---
+++ src/main/java/com/example/demo/domain/Employee.java
@@ -0,0 +1,60 @@
+package com.example.demo.domain;
+
+import java.util.*;
+
+public class Employee {
+	
+	private int id;
+	private String lastName;
+	private String firstName;
+
+	public Employee() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", lastName=" + lastName + ", firstName=" + firstName + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(firstName, id, lastName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(firstName, other.firstName) && id == other.id && Objects.equals(lastName, other.lastName);
+	}
+
+}
